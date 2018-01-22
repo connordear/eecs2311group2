3724b5cad478d24a5fa24cab12b4a3f00ba66bd6
hasibr <hasibur.r3@gmail.com>
2018-01-22 12:42:30 -0500

    Added file dialog for choosing scenario

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,24 @@
 package enamel;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	    
+    	    //Create a file chooser
+    	    final JFileChooser fc = new JFileChooser();
+    	    //In response to a button click:
+    	    int returnVal = fc.showOpenDialog(null);
+
+    	       if (returnVal == JFileChooser.APPROVE_OPTION) {
+    	           String fileName = fc.getSelectedFile().getPath();
+    	           s.setScenarioFile(fileName);
+    	       }
+    	    
+    	    //s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
 }
