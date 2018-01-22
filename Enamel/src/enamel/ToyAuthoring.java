61c1d7613bcd7748602b6358e42025cd761591b5
hasibr <hasibur.r3@gmail.com>
2018-01-22 13:40:39 -0500

    Updated ToyAuthoring, includes accessibility changes

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,23 +1,33 @@
 package enamel;
 
-import java.io.File;
-
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
     	    
-    	    //Create a file chooser
+    	    //Create a file chooser and set dialog box name (screen reader seems to only read dialog box name)
     	    final JFileChooser fc = new JFileChooser();
-    	    //In response to a button click:
+    	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+    	    	    "Text File", "txt");
+    	    fc.setFileFilter(filter);
+    	    fc.setDialogTitle("Open Scenario File");
+    	    
+    	    // Set accessible descriptions for screen reader (screen reader is not reading these)
+    	    fc.getAccessibleContext().setAccessibleName("File chooser dialog box");
+    	    fc.getAccessibleContext().setAccessibleDescription("Please choose a scenario file from this file dialog box.");
+    	    
+    	    // Open file dialog box and get chosen scenario file name
     	    int returnVal = fc.showOpenDialog(null);
 
-    	       if (returnVal == JFileChooser.APPROVE_OPTION) {
-    	           String fileName = fc.getSelectedFile().getPath();
-    	           s.setScenarioFile(fileName);
-    	       }
+    	    if (returnVal == JFileChooser.APPROVE_OPTION) {
+    	    	String fileName = fc.getSelectedFile().getPath();
+    	    	
+    	    	// Call to begin drawing UI
+    	        s.setScenarioFile(fileName);
+    	    }
     	    
     	    //s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
